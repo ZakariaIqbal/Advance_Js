5ee961c460b742cccf496410fd9fe5a78499f4e0
ZakariaIqbal <zakaria00sakib@gmail.com>
2020-03-19 01:55:10 +0600

    nullvsundefiend

---
+++ undefiend&null.js
@@ -0,0 +1,30 @@
+/*Undefiend 
+when you defiend a as unbdefiend.
+when you didn't defiend the variable.
+When you didn't return the function value. 
+when you didn't pass the paramater to the function.
+when you call a proparty from an object that isn't in defiend in the object;
+when you call a element from an array that isn't present .
+*/
+//null is shown then you defiend the variable as null or empty;
+
+
+let pakhi;
+    console.log (pakhi);
+
+function add (num1,num2)
+{
+    console.log (num1 +num2);
+}
+
+const result = add(13,14);
+    console.log (result);
+
+const boy ={name :"Zakaria" , reg :1014};
+    console.log(boy.gf);
+
+ const oy =undefined;   
+    console.log(oy);
+
+let mn = [ 13,43,553,21];
+    console.log(mn[11]);    
